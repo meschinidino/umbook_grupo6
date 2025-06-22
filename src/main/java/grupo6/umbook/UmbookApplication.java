7d4c1a200dbd78c43a722269d3cad519092aaed6
dino <francodmsoloing@gmail.com>
2025-06-22 18:57:44 -0300

    Initial commit of project files

---
+++ src/main/java/grupo6/umbook/UmbookApplication.java
@@ -0,0 +1,13 @@
+package grupo6.umbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UmbookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UmbookApplication.class, args);
+    }
+
+}
